2d8babdf6972b94960b4e97f6a909c6e8fac0502
aqu <84774626+aquar1us1337@users.noreply.github.com>
2022-12-21 03:43:00 +0300

    Delete constants.js

--- fingerprint-generator/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MISSING_VALUE_DATASET_TOKEN = exports.STRINGIFIED_PREFIX = void 0;
-exports.STRINGIFIED_PREFIX = '*STRINGIFIED*';
-exports.MISSING_VALUE_DATASET_TOKEN = '*MISSING_VALUE*';
-//# sourceMappingURL=constants.js.map
