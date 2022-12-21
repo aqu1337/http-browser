2ad72f51f1b24c2cfc6b79078d859ab460054b66
aqu <84774626+aquar1us1337@users.noreply.github.com>
2022-12-21 03:35:29 +0300

    Delete constants.js

--- fingerprint-injector/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UTILS_FILE_NAME = void 0;
-exports.UTILS_FILE_NAME = 'utils.js';
-//# sourceMappingURL=constants.js.map
